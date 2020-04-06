e3c970411cce96830b53f7757db161b80b43a522
worldblue <khj20825@gmail.com>
2020-04-06 16:52:25 +0900

    시큐리티 코드 추가

--- src/main/java/kr/han/toy/reservation/configuration/SecurityConfig.java
+++ src/main/java/kr/han/toy/reservation/configuration/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,11 +19,33 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import javax.sql.DataSource;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors().and()
+                .httpBasic().disable()
+                .authorizeRequests()
+                    .antMatchers("favicon.ico", "/login", "/test").access("hasRole('ADMIN')")
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/siginIn")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/logout")
+                    .permitAll()
+                .and()
+                .csrf().disable();
+    }
 
     @Autowired
     public void configureGlobal( AuthenticationManagerBuilder auth )  throws Exception {
@@ -36,10 +60,9 @@ public class SecurityConfig {
     /**
      * In-memory Configuration
      * @return
-     * @throws Exception
      */
     @Bean
-    public UserDetailsService userDetailsService() throws Exception {
+    public UserDetailsService userDetailsService() {
         // ensure the passwords are encoded proØperly
         UserDetails users = User
                         .withUsername("test")
@@ -52,6 +75,11 @@ public class SecurityConfig {
         return manager;
     }
 
+
+    /**
+     *  패스워드 암호화(random salt)
+     * @return
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
